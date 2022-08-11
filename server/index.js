12d4bd2b6b444f2906feabb3fa868dfaf474311f
Swadhin chand patro <schandpatro@paypal.com>
2022-08-11 16:02:46 +0530

    server utilities

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const router = require("./routes");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require('dotenv').config()
 
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
 app.use("/api", router);
 
 const port = process.env.PORT || 5000;
